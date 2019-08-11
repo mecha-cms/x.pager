6e552b2278696e2f68ffcbf3848fb8b4c306256d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-08-11 22:07:45 +0700

    Update title.php

--- pager/engine/r/content/pager/title.php
+++ pager/engine/r/content/pager/title.php
@@ -1,19 +1,19 @@
 <nav class="pager pager-title">
   <span>
   <?php if ($_prev = $pager->prev): ?>
-  <a href="<?php echo $_prev; ?>" rel="prev"><?php echo $site->is('page') ? (new Page(PAGE . DS . Path::R($_prev, $url, DS) . '.page'))->title : $language->prev; ?></a>
+  <a href="<?php echo strtr($_prev, ['&' => '&amp;']); ?>" rel="prev"><?php echo $site->is('page') ? (new Page(PAGE . DS . Path::R($_prev, $url, DS) . '.page'))->title : $language->prev; ?></a>
   <?php else: ?>
   <b><?php echo $language->prev; ?></b>
   <?php endif; ?>
   </span> <span>
   <?php if ($site->has('parent')): ?>
-  <a href="<?php echo $parent->url; ?>"><?php echo $parent->title; ?></a>
+  <a href="<?php echo $parent->url . $url->query('&amp;') . $url->hash; ?>"><?php echo $parent->title; ?></a>
   <?php else: ?>
   <a href="<?php echo $url; ?>"><?php echo $language->home; ?></a>
   <?php endif; ?>
   </span> <span>
   <?php if ($_next = $pager->next): ?>
-  <a href="<?php echo $_next; ?>" rel="prev"><?php echo $site->is('page') ? (new Page(PAGE . DS . Path::R($_next, $url, DS) . '.page'))->title : $language->next; ?></a>
+  <a href="<?php echo strtr($_next, ['&' => '&amp;']); ?>" rel="prev"><?php echo $site->is('page') ? (new Page(PAGE . DS . Path::R($_next, $url, DS) . '.page'))->title : $language->next; ?></a>
   <?php else: ?>
   <b><?php echo $language->next; ?></b>
   <?php endif; ?>
