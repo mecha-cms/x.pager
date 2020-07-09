1ee12b91c56caf9937fd834db192b5292d0826ac
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-07-09 08:54:39 +0700

    Update

--- pager/engine/r/layout/pager/steps.php
+++ pager/engine/r/layout/pager/steps.php
@@ -1,12 +1,18 @@
 <?php $pager = new PagerSteps\Pages($pages->lot, [$page->chunk ?? 5, ($url['i'] ?? 1) - 1], $parent->path ?? $page->path); ?>
 <nav class="pager pager-steps">
+  <?php if ($pager->prev): ?>
   <span>
     <?= $pager->prev(i('Previous')); ?>
   </span>
+  <?php endif; ?>
+  <?php if ($pager->steps): ?>
   <span>
     <?= $pager->steps(""); ?>
   </span>
+  <?php endif; ?>
+  <?php if ($pager->next): ?>
   <span>
     <?= $pager->next(i('Next')); ?>
   </span>
+  <?php endif; ?>
 </nav>
--- pager/engine/r/layout/pager/title.php
+++ pager/engine/r/layout/pager/title.php
@@ -1,12 +1,18 @@
 <?php $pager = new PagerTitle\Page($pages->lot, $page->path, $parent->path ?? $page->path); ?>
 <nav class="pager pager-title">
+  <?php if ($pager->prev): ?>
   <span>
     <?= $pager->prev(i('Previous')); ?>
   </span>
+  <?php endif; ?>
+  <?php if ($pager->parent): ?>
   <span>
     <?= $pager->parent(i('Home')); ?>
   </span>
+  <?php endif; ?>
+  <?php if ($pager->next): ?>
   <span>
     <?= $pager->next(i('Next')); ?>
   </span>
+  <?php endif; ?>
 </nav>
